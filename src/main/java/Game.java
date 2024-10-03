e1b7ae74c352b198f2a274302de2b7a2893b0a7b
lmcg2005 <lumicosgon.lg@gmail.com>
2024-10-03 17:55:37 +0100

    Im dumb as a rock

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,30 +4,38 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
+    private int x = 10;
+    private int y = 10;
     private final Screen screen;
     public Game() throws IOException {
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new
-                    DefaultTerminalFactory()
-                    .setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new
+                DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
 
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null); // we don't need a cursor
+        screen.startScreen(); // screens must be started
+        screen.doResizeIfNecessary(); // resize screen if necessary
     }
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() throws IOException{
         draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
+    }
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
     }
 }
